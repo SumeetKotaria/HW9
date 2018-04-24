12b3856bc3b3bb977ffbe557631bb41dc50b6a39
Sumeet Kotaria <ssk2231@instance-2.c.bamboo-parsec-193100.internal>
2018-04-24 01:36:14 +0000

    first commit

---
+++ hw9/quotes.py
@@ -0,0 +1,14 @@
+from  flask import  Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/')
+def inspiration():
+    x = randint(1,20)
+    with open("inspiration.txt", "r") as f:
+        content = f.readlines()
+    return render_template('inspiration.html', content = content[x])
+            
